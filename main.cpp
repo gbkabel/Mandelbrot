b135b5de70da50b2e4b436e4bc695b82a46bb749
gbkabel <gbkabel@yahoo.com>
2022-09-26 16:31:41 -0700

    basic structure

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
