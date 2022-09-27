2f4f72281df8cb89210277cdad545161fa3739de
gbkabel <gbkabel@yahoo.com>
2022-09-26 18:48:31 -0700

    basic info in header file

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class ComplexPlane
+{
+public:
+
+	void zoomIn();
+	void zoomOut();
+	void setCenter(Vector2f);
+	View getView();
+	void setMouseLocation(Vector2f);
+	void loadText(Text&);
+
+	void iterationsToRGB();
+private:
+	Vector2f m_mouseLocation;
+	View m_view;
+	int m_zoomCount;
+	float m_aspectRatio;
+};
+	
